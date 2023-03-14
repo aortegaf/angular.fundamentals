6fbc6473bb7fa425200086e8b35abd5e8834ea9a
aortegaf <aortegaf17@gmail.com>
2023-03-14 15:59:38 -0500

    Prettier code formatter implemetation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,75 +5,74 @@ import { Thing } from './thing.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
-  text = "I am a text defined inside the AppComponent";
+  text = 'I am a text defined inside the AppComponent';
   buttonDisabled = false;
   object = {
-    description: "I am a random object from the AppComponent",
-    name: "Random Object",
+    description: 'I am a random object from the AppComponent',
+    name: 'Random Object',
     age: 10,
-    img: "https://source.unsplash.com/random",
-    colors: ["yellow", "blue", "red"]
-  }
-  newColor = "";
+    img: 'https://source.unsplash.com/random',
+    colors: ['yellow', 'blue', 'red'],
+  };
+  newColor = '';
   things: Thing[] = [
     {
-      name: "something one",
+      name: 'something one',
       code: 1,
-      image: "https://source.unsplash.com/random"
+      image: 'https://source.unsplash.com/random',
     },
     {
-      name: "something two",
+      name: 'something two',
       code: 2,
-      image: "https://source.unsplash.com/random"
+      image: 'https://source.unsplash.com/random',
     },
     {
-      name: "something three",
+      name: 'something three',
       code: 3,
-      image: "https://source.unsplash.com/random"
+      image: 'https://source.unsplash.com/random',
     },
     {
-      name: "something four",
+      name: 'something four',
       code: 4,
-      image: "https://source.unsplash.com/random"
+      image: 'https://source.unsplash.com/random',
     },
     {
-      name: "something five",
+      name: 'something five',
       code: 5,
-      image: "https://source.unsplash.com/random"
+      image: 'https://source.unsplash.com/random',
     },
-  ]
+  ];
 
-  increaseAge(){
-    this.object.age +=1;
+  increaseAge() {
+    this.object.age += 1;
   }
 
-  decreaseAge(){
-    this.object.age -=1;
+  decreaseAge() {
+    this.object.age -= 1;
   }
 
-  toggleButton(){
+  toggleButton() {
     this.buttonDisabled = !this.buttonDisabled;
   }
 
-  changeName(event: Event){
+  changeName(event: Event) {
     const element = event.target as HTMLInputElement;
     this.object.name = element.value;
   }
 
-  eventAlert(event: Event){
+  eventAlert(event: Event) {
     alert("You just made a '" + event.type + "'");
   }
 
-  addColor(){
+  addColor() {
     this.object.colors.push(this.newColor);
-    this.newColor = "";
+    this.newColor = '';
   }
 
-  removeColor(index: number){
+  removeColor(index: number) {
     this.object.colors.splice(index, 1);
   }
 }
